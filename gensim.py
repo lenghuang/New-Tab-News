494cdc2906a7ac1e325af69f678b8ecaa421fd31
Len Huang <lenhuang720@gmail.com>
2020-03-19 23:37:39 -0400

    Random test files for summarization

---
+++ gensim.py
@@ -0,0 +1,24 @@
+# Code Taken From
+# https://towardsdatascience.com/easily-scrape-and-summarize-news-articles-using-python-dfc7667d9e74
+
+from gensim.summarization import summarize
+
+
+def read_article(file_name):
+    file = open(file_name, "r")
+    filedata = file.readlines()
+    article = filedata[0].split(". ")
+    sentences = []
+
+    for sentence in article:
+        sentences.append(sentence.replace("[^a-zA-Z]", " "))
+    return ". ".join(sentences)
+
+
+def get_ratio(n):
+    ratio = 0.001 if n == 0 else 200 / n
+    return ratio
+
+
+text = read_article('npr.txt')
+summary = summarize(text, get_ratio(len(text)))
